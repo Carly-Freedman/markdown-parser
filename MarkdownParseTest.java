8816b5c81064d74b4bf4644371d2905973edc1da
Carly-Freedman <freedmcb0023@student.laccd.edu>
2022-05-24 13:13:25 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -6,10 +6,10 @@ import java.util.List;
 import java.io.IOException;
 
 public class MarkdownParseTest { 
-    @Test 
-    public void addition() { 
-    assertEquals(2, 1 + 1); 
-    } 
+  //  @Test 
+  //  public void addition() { 
+  //  assertEquals(2, 1 + 1); 
+  //  } 
     @Test
     public void test1() throws IOException
     {
